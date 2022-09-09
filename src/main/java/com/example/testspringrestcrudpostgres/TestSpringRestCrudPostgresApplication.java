c8c99d1cc63ff8340d9e352db97878d71e124225
lmmortal <Lamixjr@icloud.com>
2022-09-09 03:03:07 +0300

    Update 3

--- src/main/java/com/example/testspringrestcrudpostgres/TestSpringRestCrudPostgresApplication.java
+++ src/main/java/com/example/testspringrestcrudpostgres/TestSpringRestCrudPostgresApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestSpringRestCrudPostgresApplication {
 
+    //master
     public static void main(String[] args) {
         SpringApplication.run(TestSpringRestCrudPostgresApplication.class, args);
     }
